15b4a5f9f540f9885abd4ca0cb0fa607a42a065c
mtracewicz <m.tracewicz@gmail.com>
2019-05-13 16:34:07 +0200

    Done version

--- src/Client.java
+++ src/Client.java
@@ -1,87 +1,114 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class Client {
-        private boolean close;
-        private String username;
+public class Client{
+    private Socket server;
+    private boolean exit;
+    private String username;
 
-        private void getUserName(){
-            try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-                System.out.println("Username:");
-                username = in.readLine();
-            }catch(Exception e){
-                e.printStackTrace(System.out);
+    private Client(Socket server) {
+        this.server = server;
+        this.exit = false;
+    }
+
+    private String stripNullChars(String toFormat){
+        StringBuilder builder = new StringBuilder();
+        for (Character c:toFormat.toCharArray()) {
+            if((int)c != 0){
+                builder.append(c);
             }
         }
+        toFormat = builder.toString();
+        return toFormat;
+    }
 
-        private void doTheWork() {
+    private String register(){
+        String preferredUsername;
+
+        do{
+            System.out.print("Username: ");
+            Scanner scanner = new Scanner(System.in);
+            preferredUsername = scanner.next();
+
             try {
-                Socket socket = new Socket("localhost", 4444);
-                BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
-                BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                boolean go;
+                DataOutputStream outputStream = new DataOutputStream(server.getOutputStream());
+                BufferedReader input = new BufferedReader(new InputStreamReader(server.getInputStream()));
 
-                do{
-                    getUserName();
-                    out.writeBytes(username);
-                    String checker = socketIn.readLine();
-                    System.out.println(checker);
-                    go = !checker.equals("Username already in use!");
-                }while(!go);
+                outputStream.writeChars(preferredUsername+"\n");
+                preferredUsername = input.readLine();
 
+                preferredUsername = this.stripNullChars(preferredUsername);
 
-                Runnable send = () -> {
-                    try {
-                        String line = "";
-                        while (!line.equals("\\exit")) {
-                            System.out.println(username+": ");
-                            line = consoleIn.readLine();
-                            line += '\n';
-                            out.writeBytes(line);
-                            out.flush();
-                        }
-                        this.close = true;
+                if(preferredUsername.equals("Taken")){
+                    System.out.println("Username already in use.");
+                    preferredUsername = null;
+                }
 
-                    } catch (Exception e) {
-                        e.printStackTrace(System.out);
-                    }
-                };
-
-                Runnable receive = () -> {
-                    try {
-                        String line;
-                        while (true) {
-                            if(!this.close){
-                                line = socketIn.readLine();
-                                if (line != null) {
-                                    System.out.println(line);
-                                }
-                            }else{
-                                break;
-                            }
-                        }
-                        socket.close();
-                    } catch (Exception e) {
-                        e.printStackTrace(System.out);
-                    }
-
-                };
-
-                Thread sendThread = new Thread(send);
-                Thread receiveThread = new Thread(receive);
-                sendThread.start();
-                receiveThread.start();
-            } catch (Exception e) {
-                e.printStackTrace(System.out);
+            }catch (IOException ioe){
+                ioe.printStackTrace(System.out);
             }
+
+        }while(preferredUsername == null);
+
+        return preferredUsername;
+    }
+
+    private void startClient(){
+        this.username = this.register();
+        System.out.println("Welcome to chat " + this.username + "!");
+        System.out.println("To exit type \"@exit\" ");
+        System.out.println("To see active users  type \"@users\" \n");
+        Runnable write = () -> {
+            try {
+                String message;
+                BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+                DataOutputStream outputStream = new DataOutputStream(server.getOutputStream());
+                while(!exit) {
+                    message = input.readLine();
+
+                    if (message.equals("@exit")) {
+                        exit = true;
+                    }
+
+                    outputStream.writeChars(message+"\n");
+                }
+            }catch (IOException ioe){
+                ioe.printStackTrace(System.out);
+            }
+
+        };
+
+        Runnable read = () -> {
+            try {
+                BufferedReader input = new BufferedReader(new InputStreamReader(server.getInputStream()));
+                String received;
+
+                while (!exit){
+                    received = input.readLine();
+                    if(received != null){
+                        System.out.println(received);
+                    }
+                }
+            }catch (IOException ioe){
+                ioe.printStackTrace(System.out);
+            }
+        };
+
+        Thread writeThread = new Thread(write);
+        Thread readThread = new Thread(read);
+
+        writeThread.start();
+        readThread.start();
+    }
+
+    public static void main(String[] args){
+        try {
+            Socket socket = new Socket("localhost", 4444);
+            Client client = new Client(socket);
+            client.startClient();
+        }catch (Exception e){
+            e.printStackTrace(System.out);
         }
-        public static void main(String[] args){
-            Client program = new Client();
-            program.close = false;
-            program.doTheWork();
-        }
+    }
 }
